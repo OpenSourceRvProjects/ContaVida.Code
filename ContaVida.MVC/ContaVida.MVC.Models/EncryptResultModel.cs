1bf9d0520420cba4c4910a3ea99acbeabc68d70a
Rolando <rolando.code@gmail.com>
2025-11-10 16:12:05 -0600

    Encrypt core

---
+++ ContaVida.MVC/ContaVida.MVC.Models/EncryptResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaVida.MVC.Models
+{
+    public class EncryptResultModel
+    {
+        public bool IsError { get; set; } = false;
+        public string ErrorMessage { get; set; }
+        public string EncodeddPassword { get; set; }
+        public string Salt { get; set; }
+    }
+}
---
+++ ContaVida.MVC/ContaVida.MVC.Security/Infraestructure/IEncryptCore.cs
@@ -0,0 +1,12 @@
+﻿using ContaVida.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaVida.MVC.Security.Infraestructure
+{
+    public interface IEncryptCore
+    {
+        Task<EncryptResultModel> RunEncrypt(string rawPassword);
+    }
+}
