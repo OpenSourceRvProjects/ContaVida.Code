4eb3a9ec41587180d117487318b740b1b4dde868
Rolando <rolando.code@gmail.com>
2025-12-20 00:27:03 -0600

    System security guards in angular legacy from ngLifeCounter

---
+++ ContaVida.MVC/contavida.mvc.client/src/app/nav-menu/Security/common.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '../../services/Storage/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonGuard  {
+
+  constructor(private storageService: LocalStorageService, private router: Router) {
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.storageService.getUserData()) {
+      return true;
+    }
+    else {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
+
+    }
+  }
+}
