a3af2e9c0788abfa2416da616ecf345408154b64
Rolando <rolando.code@gmail.com>
2025-12-20 21:31:03 -0600

    my counter component copied from ngLifeCounter

--- ContaVida.MVC/contavida.mvc.client/src/app/modules/counter/counter.module.ts
+++ ContaVida.MVC/contavida.mvc.client/src/app/modules/counter/counter.module.ts
@@ -11,12 +11,14 @@ import { ModalRelapsesComponent } from './relapses/modalRelapses';
 import { ModalDeleteCounterComponent } from './delete-counter/modalDeleteCounter';
 import { ModalEditComponent } from '../edit-counter/modalEditCounter';
 import { CounterRoutingModule } from './counter-routing.module';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { SharedModule } from '../shared/shared.module.';
 
 @NgModule({
   declarations: [
     // AddCounterComponent,
     CounterListComponent,
-    // MyCounterComponent,
+    MyCounterComponent,
     ModalEditComponent,
     ModalRelapsesComponent,
     ModalDeleteCounterComponent,
@@ -26,7 +28,7 @@ import { CounterRoutingModule } from './counter-routing.module';
     FormsModule,
     CommonModule,
     // EnvironmentModule,
-    // SharedModule,
+    SharedModule,
     NgbModule
   ]
 })
