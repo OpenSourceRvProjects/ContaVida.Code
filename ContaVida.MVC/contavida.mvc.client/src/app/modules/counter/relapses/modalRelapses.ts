1c4ccc24c2aad7f1a7c85af299bf8298c6bc38bc
Rolando <rolando.code@gmail.com>
2026-02-01 01:06:46 -0600

    Downlod pdf Placeholder for document verification

--- ContaVida.MVC/contavida.mvc.client/src/app/modules/counter/relapses/modalRelapses.ts
+++ ContaVida.MVC/contavida.mvc.client/src/app/modules/counter/relapses/modalRelapses.ts
@@ -3,6 +3,7 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { IEventCounterItemModel } from '../../../Models/EventCounter/IEventCounterItemModel';
 import { RelapsesDataModel } from '../../../Models/Relapses/IRelapseDetailModel';
 import { RelapsesService } from '../../../services/Relapses/relapses.service';
+import { EventService } from '../../../services/Events/event.service';
 @Component({
   selector: 'relapses-modal',
   templateUrl: './modalRelapses.html',
@@ -12,7 +13,9 @@ import { RelapsesService } from '../../../services/Relapses/relapses.service';
 export class ModalRelapsesComponent implements OnInit {
 
   @Input() counterEvent: IEventCounterItemModel = <IEventCounterItemModel>{};
-  constructor(public activeModal: NgbActiveModal, private relapseService: RelapsesService) { }
+  constructor(public activeModal: NgbActiveModal, private relapseService: RelapsesService,
+    private eventService: EventService
+  ) { }
 
   processing: boolean = false;
   relapsesData: RelapsesDataModel = <RelapsesDataModel>{};
@@ -24,6 +27,22 @@ export class ModalRelapsesComponent implements OnInit {
     }, error: (err)=>{
 
     }})
+
+  }
+  
+  donwoloadConstancyDocument(){
+      this.eventService.getPDF(this.counterEvent.id).
+    subscribe((base64: any) => {
+      const src = `data:application/pdf;base64,${base64}`;
+      const link = document.createElement("a")
+      link.href = src
+
+      link.download = "Constancia " + this.counterEvent.eventName;
+      link.click()
+
+      link.remove()
+    }, (errorEvent: any) => {
+    });
 
   }
 
